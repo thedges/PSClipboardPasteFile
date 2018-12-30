8bdab7a3f04aa95eba39c2cbcaf98621a0eec819
Troy Hedges <thedges@salesforce.com>
2018-12-30 08:47:47 -0600

    Initial load

---
+++ src/aura/PSClipboardPasteFile/PSClipboardPasteFileController.js
@@ -0,0 +1,61 @@
+({
+    init: function (component, event, helper) {
+        var imageSize = component.get('v.imageSize');
+        
+        window.addEventListener (
+            'paste',
+            function (pasteEvent) {
+                if(pasteEvent.clipboardData != false){
+                    
+                    var items = pasteEvent.clipboardData.items;
+                    
+                    if(items != undefined)
+                    {
+                        for (var i = 0; i < items.length; i++) {
+                            // Skip content if not image
+                            if (items[i].type.indexOf("image") == -1) continue;
+                            // Retrieve image on clipboard as blob
+                            var imageBlob = items[i].getAsFile();
+                            
+                            console.log('imageBlob size 1 = ' + imageBlob.size);
+                            console.log('imageBlob type 1 = ' + imageBlob.type);
+                            
+                            imageConversion.compressAccurately(imageBlob, imageSize)
+                            .then(function(res) {
+                                console.log(res);
+                                console.log('size = ' + res.size);
+                                console.log('type = ' + res.type);
+                                console.log('name = ' + res.name);
+                                
+                                component.set('v.fileData', res);
+                                var myimg = document.getElementById ('myimg');
+                                myimg.src = URL.createObjectURL (res);
+                            });
+                            
+                        }
+                    }
+                }
+            },
+            false
+        );
+    },
+    saveFile : function (component, event, helper) {
+        var fileData = component.get('v.fileData');
+        var title = component.get('v.title');
+        var base64Data = null;
+        
+        helper.showSpinner(component);
+        helper.getBase64(fileData).then(function(data) 
+                                        {
+                                            console.log('base64=' + data); 
+                                            base64Data = data;
+                                            helper.upload(component, title, fileData, base64Data);
+                                            
+                                            //$A.get('e.force:refreshView').fire();
+                                        });
+        
+    },
+    clear : function (component, event, helper) {
+        component.set('v.fileData', null);
+    }
+})
---
+++ src/aura/PSClipboardPasteFile/PSClipboardPasteFileHelper.js
@@ -0,0 +1,71 @@
+({
+    upload: function(component, title, file, base64Data) {
+        var self = this;
+        if (title == null) title = '';
+        
+        var action = component.get("c.saveAttachment"); 
+        action.setParams({
+            parentId: component.get("v.recordId"),
+            title: title,
+            base64Data: base64Data, 
+            contentType: file.type
+        });
+        action.setCallback(this, function(a) {
+            var state = a.getState();
+            if (state === "SUCCESS") {
+                component.set('v.fileData', null);
+                $A.get('e.force:refreshView').fire();
+            } else {
+                self.handleErrors(component, a.getError());
+            }
+            self.hideSpinner(component);
+            
+        });
+        $A.enqueueAction(action); 
+    },
+    handleErrors: function (component, errors) {
+        // Configure error toast
+        let toastParams = {
+            title: "Error!",
+            message: "Unknown error", // Default error message
+            type: "error",
+            mode: "sticky"
+        };
+        // Pass the error message if any
+        if (errors && Array.isArray(errors) && errors.length > 0) {
+            toastParams.message = errors[0].message;
+        }
+        else
+        {
+            //toastParams.message = errors;
+        }
+        // Fire error toast
+        let toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams(toastParams);
+        toastEvent.fire();
+        
+    },
+    showSpinner:function(component){
+        component.set("v.IsSpinner",true);
+    },
+    hideSpinner:function(component){
+        component.set("v.IsSpinner",false);
+    },
+    getBase64 : function(file) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+            console.log('result=' + reader.result);
+            let encoded = reader.result.replace(/^data:(.*;base64,)?/, '');
+            if ((encoded.length % 4) > 0) 
+            {
+              encoded += '='.repeat(4 - (encoded.length % 4));
+            }
+            resolve(encoded);
+        };
+        reader.onerror = error => reject(error);
+      });
+    }   
+
+})
